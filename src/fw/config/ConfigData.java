24a9c254d51ff56e526a0afb69dc2a6d8db7a6be
loucass003 <loucass003@gmail.com>
2016-12-04 10:59:15 +0100

    Adding deathlevel in config

--- src/fw/config/ConfigData.java
+++ src/fw/config/ConfigData.java
@@ -8,19 +8,23 @@ import java.util.List;
 /**
  * Created by loucass003 on 26/11/16.
  */
-public class ConfigData {
-
+public class ConfigData
+{
     public List<ConfigLocation> gameSpawns;
     public List<ConfigPlatform> platforms;
+    public ConfigLocation spectatorLoc;
+    public int deathLevel;
 
     public ConfigData()
     {
         gameSpawns = new ArrayList<>();
         platforms = new ArrayList<>();
+        spectatorLoc = null;
+        deathLevel = -1;
     }
 
     public boolean minimumConfigIsSet()
     {
-        return gameSpawns.size() > 0 && platforms.size() > 0;
+        return deathLevel >= 0 && spectatorLoc != null && gameSpawns.size() > 0 && platforms.size() > 0;
     }
 }
